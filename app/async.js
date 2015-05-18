3c510cf2848a27b75881f98f804c2a9e7c090e0a
Valentino Espinoza <xvalentino13@gmail.com>
2015-05-17 23:22:06 -0600

    async is weird but the tests are passing

--- app/async.js
+++ app/async.js
@@ -1,11 +1,29 @@
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
-  },
+      var dfd = $.Deferred();
+      setTimeout(function() {
+            dfd.resolve(value);
+          }, 10);
+      return dfd.promise();
+    },
 
   manipulateRemoteData : function(url) {
-
-  }
+      var dfd = $.Deferred();
+  
+      $.ajax(url).then(function(resp) {
+            var people = $.map(resp.people, function(person) {
+                    return person.name;
+                  });
+            dfd.resolve(people.sort());
+          });
+  
+      return dfd.promise();
+    }
 };
